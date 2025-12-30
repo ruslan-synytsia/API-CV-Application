c28cf8fb94b7a60af387428b1483657d4e0035f5
Ruslan <64065765+ruslan-synytsia@users.noreply.github.com>
2025-12-30 16:09:06 +0100

    Mailer_fix

--- Components/Services/mailerService.js
+++ Components/Services/mailerService.js
@@ -4,45 +4,46 @@ const fs = require("fs");
 const ejs = require("ejs");
 
 class Mailer {
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 20000,
-    });
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: Number(process.env.SMTP_PORT),
+            secure: false,
+            requireTLS: true,
+            auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+            },
+            connectionTimeout: 10000,
+            greetingTimeout: 10000,
+            socketTimeout: 20000,
+        });
 
-    this.transporter.verify()
-      .then(() => console.log("SMTP OK"))
-      .catch((e) => console.error("SMTP FAIL", e));
-  }
+        this.transporter.verify()
+            .then(() => console.log("SMTP OK"))
+            .catch((e) => console.error("SMTP FAIL", e));
+    }
 
-  async sendMail({ from, to, email, username, subject, message, temp }) {
-    const template = fs.readFileSync(
-      path.join(__dirname, `../../Templates/${temp}.ejs`),
-      "utf-8"
-    );
+        async sendMail({ from, to, email, username, subject, message, temp }) {
+            const template = fs.readFileSync(
+                path.join(__dirname, `../../Templates/${temp}.ejs`),
+                "utf-8"
+            );
 
-    const html = ejs.render(template, { from, email, username, subject, message });
+        const html = ejs.render(template, { from, email, username, subject, message });
 
-    const mailOptions = {
-      from: from || process.env.SMTP_USER,
-      to,
-      subject,
-      html,
-      replyTo: email || undefined,
-    };
+        const mailOptions = {
+            from: from || process.env.SMTP_USER,
+            to,
+            subject,
+            html,
+            replyTo: email || undefined,
+        };
 
-    const info = await this.transporter.sendMail(mailOptions);
-    console.log("Message sent:", info.messageId);
-    return info;
-  }
+        const info = await this.transporter.sendMail(mailOptions);
+        console.log("Message sent:", info.messageId);
+        return info;
+    }
 }
 
 module.exports = Mailer;
