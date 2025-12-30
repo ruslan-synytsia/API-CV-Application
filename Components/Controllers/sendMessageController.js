846e410c01d500e2e4979524b84846c8e744b55f
Ruslan <64065765+ruslan-synytsia@users.noreply.github.com>
2025-12-30 15:03:15 +0100

    Send_message_fix

--- Components/Controllers/sendMessageController.js
+++ Components/Controllers/sendMessageController.js
@@ -2,43 +2,38 @@ const formDataService = require('./../Services/formDataService');
 const Mailer = require('../Services/mailerService');
 
 class sendMessageController {
-    async sendMessage (req, res) {
-        const { username, email, message } = req.body;
-        try {
-            await formDataService.createMessage({ username, email, message });
-            try {
-                await new Mailer().sendMail(
-                    {
-                        from: '',
-                        to: process.env.SMTP_USER,
-                        email,
-                        username,
-                        subject: "Message from CV",
-                        message,
-                        temp: 'mail-template'
-                    });
-            } catch (err) {
-                console.error(err)
-            }
-            try {
-                await new Mailer().sendMail(
-                    {
-                        from: 'Ruslan Synytsia <synytsiaruslan@gmail.com>',
-                        to: email,
-                        email: '',
-                        username,
-                        subject: "Thank you for your offer!",
-                        message: '',
-                        temp: 'response-mail-template'
-                    });
-            } catch (err) {
-                console.error(err)
-            }
-            res.send('Message sent successfully!')
-        } catch (e) {
-            console.log(e)
-        }
+  async sendMessage(req, res) {
+    const { username, email, message } = req.body;
+
+    try {
+      const saved = await formDataService.createMessage({ username, email, message });
+
+        new Mailer().sendMail({                         
+            from: '',
+            to: process.env.SMTP_USER,
+            email,
+            username,
+            subject: "Message from CV",
+            message,
+            temp: 'mail-template' 
+        }).catch(console.error);
+        
+        new Mailer().sendMail({ 
+            from: 'Ruslan Synytsia <synytsiaruslan@gmail.com>',
+            to: email,
+            email: '',
+            username,
+            subject: "Thank you for your offer!",
+            message: '',
+            temp: 'response-mail-template'
+        }).catch(console.error);
+
+      return res.status(201).json({ ok: true, id: saved._id });
+    } catch (e) {
+      console.error("sendMessage failed:", e);
+      return res.status(500).json({ ok: false, error: "Mongo save failed" });
     }
+  }
 }
 
 module.exports = new sendMessageController();
--- Components/Services/formDataService.js
+++ Components/Services/formDataService.js
@@ -1,27 +1,16 @@
 const Message = require('../Models/Message.js');
-// const mailService = require('./mailService.js');
-// const tokenService = require('./tokenService.js');
-// const UserDTO = require('./../dtos/user_dto.js');
-// const uuid = require('uuid');
-// const bcrypt = require('bcrypt');
-// const fs = require('fs');
 
 class formDataService {
-    async createMessage(data) {
-        const {username, email, message} = data;
-        try {
-            const newMessage = await Message.create({
-                username,
-                email,
-                message,
-            });
-            console.log('New message created:\n', newMessage);
-            return newMessage;
+try {
+    const newMessage = await Message.create({ username, email, message });
+    console.log("DB:", Message.db.name);
+    console.log("Collection:", Message.collection.name);
+    console.log('New message created:\n', newMessage);
+    return newMessage;
         } catch (err) {
-            console.error(err);
-            return err;
+            console.error("Mongo save error:", err);
+            throw err;
         }
-    }
 }
 
 module.exports = new formDataService();
