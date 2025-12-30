de6e9488f56a34e60bbc7e628178a7de3497b521
Ruslan <64065765+ruslan-synytsia@users.noreply.github.com>
2025-12-30 17:20:27 +0100

    Mailer_fix

--- Components/Services/mailerService.js
+++ Components/Services/mailerService.js
@@ -5,15 +5,15 @@ const ejs = require("ejs");
 
 class Mailer {
   constructor() {
-        this.transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: process.env.SMTP_PORT,
-      secure: false,
-      requireTLS: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
+      this.transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: process.env.SMTP_PORT,
+        secure: false,
+        requireTLS: true,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
     });
 
     // 🔍 Диагностика при старте
